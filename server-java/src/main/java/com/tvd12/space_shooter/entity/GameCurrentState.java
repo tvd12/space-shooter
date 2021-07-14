5f39c0011759a834e2f01e20a4cbdee28c822f38
Dung Ta Van <itprono3@gmail.com>
2021-07-14 21:45:43 +0700

    fix mapping

--- server-java/src/main/java/com/tvd12/space_shooter/entity/GameCurrentState.java
+++ server-java/src/main/java/com/tvd12/space_shooter/entity/GameCurrentState.java
@@ -1,6 +1,7 @@
 package com.tvd12.space_shooter.entity;
 
 import com.tvd12.ezyfox.annotation.EzyId;
+import com.tvd12.ezyfox.database.annotation.EzyCollection;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,6 +9,7 @@ import lombok.Setter;
 
 @Getter
 @Setter
+@EzyCollection
 @NoArgsConstructor
 @AllArgsConstructor
 public class GameCurrentState {
